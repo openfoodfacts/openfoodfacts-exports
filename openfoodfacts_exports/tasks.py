a3b6a1d63cce6e21d62721e1af872782ecea4a20
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2024-11-08 16:26:05 +0100

    fix: increase job timeout

--- openfoodfacts_exports/tasks.py
+++ openfoodfacts_exports/tasks.py
@@ -20,10 +20,12 @@ def export_job(flavor: Flavor) -> None:
             export_parquet,
             dataset_path,
             PARQUET_DATASET_PATH,
+            job_timeout="1h",
         )
         high_queue.enqueue(
             generate_mobile_app_dump,
             PARQUET_DATASET_PATH,
             MOBILE_APP_DUMP_DATASET_PATH,
             depends_on=export_parquet_job,
+            job_timeout="1h",
         )
