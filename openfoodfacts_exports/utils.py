eec86fa25c3fe4cc65a7df68b61f2d01634dd6f4
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2026-06-11 11:09:31 +0200

    fix: lint utils.py

--- openfoodfacts_exports/utils.py
+++ openfoodfacts_exports/utils.py
@@ -10,7 +10,6 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 
 from openfoodfacts_exports import settings
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -45,9 +44,10 @@ def get_minio_client() -> Minio:
 
 def timer(func):
     def wrapper(*args, **kwargs):
-        timestamp= time.time()
+        timestamp = time.time()
         output = func(*args, **kwargs)
         latency = time.time() - timestamp
         logger.info(f"Latency of {func.__name__}: {latency:.2f} seconds")
         return output
+
     return wrapper
